505834b519a569080997dca4957ad7b0d94b08f3
Uranus_18C5E <mc2748246086@outlook.com>
2025-04-19 15:37:24 +0800

    Add files via upload

---
+++ 别删就对了是头文件.h
@@ -0,0 +1,56 @@
+#include <stdlib.h>
+#include<iostream>
+#include<ctime>
+#include<windows.h>
+void punish()
+{
+    srand(time(0));
+    int chose=rand()%5;
+    switch(chose)
+    {
+        case 0:
+        system("start https://www.bilibili.com/video/BV1GJ411x7h7/?spm_id_from=333.337.search-card.all.click&vd_source=597e466ef710fffe8014182bede215d8");
+        break;
+        case 1:
+        system("cls");
+        system("color 1F");
+		std::cout<<"No problems has been detected and windows havent been shut down to prevent demage\nto your computer\n"
+        <<"\n\nNo process or thread cruial system oprration has unexpectedly exited or\nbeen terminted\n\n"
+        <<"If this is the first time you\'ve seen real stop error screen like this on your computer,\n"<<"restart your computer. If screen really like this appers again"
+        <<"instead message box like this,\nfollow these steps:"<<"\n\n"<<"Check to make sure any new hardware or software is properly installed.\n"
+        <<"If this is a new installation, ask your hard ware or software manufactuer\nfor any windows updates you might need."
+        <<"\n\n"<<"If problms continue, disable or remove any newly installed hardware \nor software. Disable BIOS memory options such as cachin or shadowing.\n"
+        <<"your computer. pressF8 to select Advanced Startup Options, and then\nselect Safe Mode"
+        <<"\n\nUseless and random hexadecimal numbers:\n\n"
+        <<"*** IDK: 0x000000F1 (0x005572616E75735F, 0x0000000000018C5E, 0x0032697332746865, 0x0000000000646576)"
+        <<"\n\n\n\nNo error on your computer...\n"
+        <<"Plese remeber it is just a joke...\n"
+        <<"I am sorry for I do not know QT\n"
+        <<"Another random DEC number:64";
+        Beep(659,450);
+        Beep(659,220);
+        Beep(880,380);
+        Beep(784,500);
+        Beep(698,400);
+        Beep(659,400);
+        Beep(659,280);
+        Beep(659,400);
+        Beep(784,550);
+        Beep(698,370);
+        Beep(659,200);
+        Beep(587,350);
+        system("pause");
+        system("color 0F");
+        break;
+        case 2:
+        system("start https://github.com");
+        break;
+        case 3:
+        system("start https://www.bilibili.com/video/BV12X4y1j76P/?share_source=copy_web&vd_source=d1f709afee7dedcab68ca6153fa9e94b");
+        break;
+        case 4:
+        system("start https://www.bilibili.com/video/BV1NE411D7vA?spm_id_from=333.788.recommend_more_video.2&vd_source=597e466ef710fffe8014182bede215d8");
+        break;
+    }
+    
+}
